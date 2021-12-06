c06664ae16ecbf8f65f8c3ae277554a714bc2dcd
maodreamer <maodreamer@gmail.com>
2021-12-06 16:44:22 +0100

    Add connectedness test

--- test-tram.py
+++ test-tram.py
@@ -1,6 +1,9 @@
+from datetime import time
+import math
 from hypothesis import given, strategies as st
 import hypothesis
 import tram
+import json
 
 gener_int = st.integers(min_value=0, max_value=100)
 alphabet = [chr(i) for i in range(65,91)]+[chr(i) for i in range(97,123)]
@@ -10,7 +13,7 @@ gener_line_stops = st.dictionaries(gener_int, gener_stops, min_size=8, max_size=
 
 @hypothesis.settings(max_examples=6)
 @given(gener_line_stops)
-def test_line(gener_line_stops):
+def test_lines(gener_line_stops):
     lines = gener_line_stops
     tramnetwork =  tram.TramNetwork(lines, {}, {}, start=None)
 
@@ -42,3 +45,33 @@ def test_stops(gener_stops):
         assert stops[stop]['lat'] == tramnetwork.stop_position(stop)[0]
         assert stops[stop]['lon'] == tramnetwork.stop_position(stop)[1]
 
+def connectedness_test():
+    '''
+    Test stop connectedness by BFS
+    '''
+    with open('./tramnetwork.json', 'r', encoding='utf-8') as jfile:
+        tramnetwork = json.load(jfile)
+    times = tramnetwork['times']
+    start = list(times.keys())[0]
+
+    queue = [start]
+    all_stops = [start]
+    while len(queue) > 0:
+        stop = queue.pop(0)
+        stops = list(times[stop].keys())
+        for _stop in stops:
+            if _stop not in all_stops:
+                queue.append(_stop)
+                all_stops.append(_stop)
+    
+    assert len(list(set(all_stops))) == len(list(times.keys()))
+    assert set(all_stops) == set(list(times.keys()))
+
+
+def main():
+    test_lines()
+    test_stops()
+    connectedness_test()
+
+if __name__ == '__main__':
+        main()
