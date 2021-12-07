282f9f0b8360f44b0857d2a06ccc70acafbfb908
Zhuqi Xiao <zqxiao.shawn@qq.com>
2021-12-07 17:37:46 +0100

    mainly modified remove_lines()

--- tram.py
+++ tram.py
@@ -18,27 +18,36 @@ class TramStop:
               get_position() : Get the position of this stop
               set_position(lat, lon) : Set position of this stop
     ''' 
-    def __init__(self, name, lines = [], lat = 0, lon = 0):
+    def __init__(self, name, lines = [], lat = None, lon = None):
         self._name = str(name)
         self._lines = lines
-        self.set_position( lat, lon)
+        self._position = tuple()
+        self.set_position(lat, lon)
 
     def add_line(self, line):
         line = str(line)
         if line not in self._lines:
             self._lines.append(line)
+    
+    def remove_line(self, line):
+        line = str(line)
+        if line in self._lines:
+            self._lines.remove(line)
 
     def get_lines(self):
-        return self._lines
+        if self._lines:
+            return self._lines
 
     def get_name(self):
         return self._name
 
     def get_position(self):
-        return self._position
+        if self._position:
+            return self._position
 
     def set_position(self, lat, lon):
-        self._position = (float(lat), float(lon))
+        if lat != None and lon != None:
+            self._position = (float(lat), float(lon))
 
 
 class TramLine:
@@ -52,16 +61,15 @@ class TramLine:
               get_stops() : Get all stops where line pass
 
     ''' 
-    def __init__(self, num, stops = []):
+    def __init__(self, num, stops):
         self._number = str(num)
-        self._stops = stops
+        self._stops = list(stops)
     
     def get_number(self):
         return self._number
 
     def get_stops(self):
-        return list(self._stops)
-
+        return self._stops
 
 
 class TramNetwork(gr.WeightedGraph):
@@ -83,31 +91,34 @@ class TramNetwork(gr.WeightedGraph):
               stop_position(): Get the position of this stop, Retrun tuple
               transition_time(a,b): Get the time cost from stop a to stop b
     ''' 
-    def __init__(self, lines, stops, times, start):
-        super().__init__(start)
+    def __init__(self, lines=None, stops=None, times=None):
+        super().__init__()
 
-        self._linedict = {}
+        self._linedict = dict()
         if lines:
             for line in lines:
                 self._linedict[str(line)] = TramLine(line, lines[line])
 
-
-        self._stopdict = {}
+        self._stopdict = dict()
         if stops:
             for stop in stops:
-
                 _tmp_stop_line = []
                 for line in lines:
                     if stop in self._linedict[line].get_stops():
                         _tmp_stop_line.append(line)
-                
-                self._stopdict[stop] = TramStop(stop, lines = _tmp_stop_line, lat = stops[stop]['lat'], lon = stops[stop]['lon'])
+                self._stopdict[str(stop)] = TramStop(stop, lines=_tmp_stop_line, lat=stops[stop]['lat'], lon=stops[stop]['lon'])
 
-
-        self._timedict = {}
+        self._timedict = dict()
         if times:
             self._timedict = times
-            
+        # Initialize weighted graph
+        for stop in self.all_stops():
+            self.add_vertex(stop)
+        for stop1 in self._timedict:
+            for stop2 in self._timedict[stop1]:
+                self.add_edge(stop1, stop2)
+                self.set_weight(stop1, stop2, self._timedict[stop1][stop2])
+                
     
     def all_lines(self):
         return list(self._linedict.keys())
@@ -117,24 +128,25 @@ class TramNetwork(gr.WeightedGraph):
 
     def extreme_positions(self):
         pass
-
-    # def geo_distance(self,a,b):
-    #     return td.distance_between_stops(self._stopdict[a], self._stopdict[b])
     
-    def geo_distance(self,a,b):
+    def geo_distance(self, a, b):
         a = str(a)
         b = str(b)
 
-        if a not in list(self._stopdict.keys()) or str(b) not in list(self._stopdict.keys()):
+        if a not in list(self._stopdict.keys()) or b not in list(self._stopdict.keys()):
             return "unknown arguments"
         if a == b:
             return("Please enter two different stops!")
+        
+        lat1, lon1 = self.stop_position(a)
+        lat2, lon2 = self.stop_position(b)
+        
         Earth_Radius = 6371.009
         one_degree = math.pi/180
-        lat1 = self._stopdict[a].get_position()[0] * one_degree
-        lon1 = self._stopdict[a].get_position()[1] * one_degree
-        lat2 = self._stopdict[b].get_position()[0] * one_degree
-        lon2 = self._stopdict[b].get_position()[1] * one_degree
+        lat1 *= one_degree
+        lon1 *= one_degree
+        lat2 *= one_degree
+        lon2 *= one_degree
 
         dlon = lon2 - lon1
         dlat = lat2 - lat1
@@ -143,57 +155,64 @@ class TramNetwork(gr.WeightedGraph):
         distance = Earth_Radius * math.sqrt(dlat**2 + (math.cos(lat_m) * dlon)**2)
         return round(distance, 3)
 
-    def line_stop(self, line):
+    def line_stops(self, line):
         line = str(line)
         if line in list(self._linedict.keys()):
             return self._linedict[line].get_stops()
         return("The line {} is not exist".format(line))
 
-
-
     def remove_lines(self, lines):
-        lines = str(lines).split(" ")
-
         for line in lines:
+            line = str(line)
+            line_stops = self.line_stops(line)
+            
+            # update _linedict, remove the whole line
             if line in list(self._linedict.keys()):
                 del self._linedict[line]
             
-            line_stops = self._linedict[line]._stops
-            for i in range(len(line_stops)):
-                if line_stops[i] in list(self._timedict.keys()):
-                    if line_stops[i+1] in list(self._timedict[line_stops[i]].keys):
-                        del self._timedict[line_stops[i]][line_stops[i+1]]
-                        self.remove_edge(line_stops[i], line_stops[i+1])
+            # update _stopdict, remove the line from stop's info
+            for stop in line_stops:
+                if line in self._stopdict[stop].get_lines():
+                    self._stopdict[stop].remove_line(line)
             
-            line_stops = line_stops[::-1]
-            for i in range(len(line_stops)):
-                if line_stops[i] in list(self._timedict.keys()):
-                    if line_stops[i+1] in list(self._timedict[line_stops[i]].keys):
-                        del self._timedict[line_stops[i]][line_stops[i+1]]
-                        self.remove_edge(line_stops[i], line_stops[i+1])
-            
-            for stop in self._stopdict:
-                if line in self._stopdict[stop]._lines:
-                    del self._stopdict._lines[stop][line]
-            
+            # update _timedict
+            # if no other lines passing through adjacent stops in the deleting line, delete time info
+            # and also delete this edge from edges
+            for i, stop in enumerate(line_stops):
+                delete = True
+                if i == len(line_stops)-2:
+                    break
+                next_stop = line_stops[i+1]
+                for other_line in self.all_lines():
+                    other_line_stops = self.line_stops(other_line)
+                    for _i, _stop in enumerate(other_line_stops):
+                        if _i == len(other_line_stops)-2:
+                            break
+                        _next_stop = other_line_stops[_i+1]
+                        if (stop == _stop and next_stop == _next_stop) or (stop == _next_stop and next_stop == _stop):
+                            delete = False
+                            break
+                    if not delete:
+                        break
+                if delete:
+                    self.remove_edge(stop, next_stop)
+                    del self._timedict[stop][next_stop]
+                    del self._timedict[next_stop][stop]
+                    
 
     def stop_lines(self, a):
-        if a:
-            if a in self._stopdict:
-                return self._stopdict[a].get_lines()
+        if a in self._stopdict:
+            return self._stopdict[a].get_lines()
         
-
     def stop_position(self, a):
-        if a:
-            if a in self._stopdict:
-                return self._stopdict[a].get_position()
+        if a in self._stopdict:
+            return self._stopdict[a].get_position()
 
     def transition_time(self, a, b):
-        if a and b:
-            if a in list(self._timedict.keys()):
-                if b in list(self._timedict[a].keys()):
-                    return 'The transition times between {} and {} is {}}'.format(a,b,self._timedict[a][b])
-                return '{} and {} are not adjacent'.format(a,b)
+        if a in list(self._timedict.keys()):
+            if b in list(self._timedict[a].keys()):
+                return self._timedict[a][b]
+            return '{} and {} are not adjacent'.format(a,b)
             
 
 
@@ -205,7 +224,6 @@ def readTramNetwork(tramfile='./tramnetwork.json'):
 
     Output: a object of tramnetwork
     ''' 
-
     with open(tramfile, 'r', encoding='utf-8') as jfile:
         tramnetwork = json.load(jfile)
 
@@ -213,19 +231,18 @@ def readTramNetwork(tramfile='./tramnetwork.json'):
     stops = tramnetwork['stops']
     times = tramnetwork['times']
 
-    tramnetwork =  TramNetwork(lines, stops, times, start=None)
-    if times:
-        for stop1 in times:
-            for stop2 in times[stop1]:
-                tramnetwork.add_edge(stop1, stop2)
-                tramnetwork.set_weight(stop1, stop2, times[stop1][stop2])
+    tramnetwork =  TramNetwork(lines, stops, times)
     return tramnetwork
 
 
 def demo():
     G = readTramNetwork()
     a, b = input('from,to ').split(',')
-    gr.view_shortest(G, a, b)
+    gr.view_shortest(G, a, b, cost=lambda u, v: G.get_weight(u, v))
+    print(G.geo_distance('Chalmers', 'Valand'))
+    # G.remove_lines(['3', '4', '5', '6', '7', '8', '9', '10', '11', '13'])
+    # gr.visualize(G, 'fdp')
+    
 
 if __name__ == '__main__':
-        demo()
+    demo()
