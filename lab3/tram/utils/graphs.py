bebde831f87f803e8899e7159e64bce188b8ed47
Zhuqi Xiao <zqxiao.shawn@qq.com>
2021-12-26 12:12:38 +0100

    Baseline+Bonus2

---
+++ lab3/tram/utils/graphs.py
@@ -0,0 +1,177 @@
+# TODO: mock-up to be replaced by your file from Lab 2
+
+import graphviz
+from queue import PriorityQueue
+
+class Graph:
+    """
+    A class of undirected graphs.
+    Internal representation:
+        {node: {'_value': None, neighbour1: {}, neighbour2: {}, ...}, ...}
+    """
+    def __init__(self, start=None):
+        "Start with an input list of edges or an empty graph, values are set to be None at first."
+        self._adjlist = dict()
+        if start:
+            for a, b in start:
+                self.add_edge(a,b)
+
+    def __len__(self):
+        "Return the length (number) of vertices."
+        return len(self._adjlist.keys())
+
+    def __getitem__(self, v):
+        "Gives the neighbours and value (more info can be added, e.g. weight) of vertex v."
+        return self._adjlist[v]
+    
+    def __str__(self):
+        "Shows the adjacency list."
+        st = [str(key)+' - '+str(self._adjlist[key]) for key in self._adjlist.keys()]
+        return '\n'.join(st)
+
+    def vertices(self):
+        "Lists all vertices."
+        return list(self._adjlist.keys())
+
+    def edges(self):
+        "Lists all edges in one direcion (sorted)."
+        edges = set()
+        for a in self._adjlist:
+            for b in self._adjlist[a]:
+                if b == '_value':
+                    continue
+                if a <= b:
+                    edges.add((a, b))
+                else:
+                    edges.add((b, a))
+        edges = list(edges)
+        edges.sort(key=lambda a: (a[0], a[1]))
+        return edges
+    
+    def add_vertex(self, a):
+        "Adds a vertex if not exist."
+        if a not in self._adjlist:
+            self._adjlist[a] = {'_value': None}
+    
+    def add_edge(self, a, b):
+        "Adds an edge, and the vertices if needed."
+        self.add_vertex(a)
+        self._adjlist[a][b] = dict()
+        self.add_vertex(b)
+        self._adjlist[b][a] = dict()
+
+    def neighbours(self, v):
+        "Lists all neighbours of v (sorted)."
+        if v in self._adjlist:
+            nbs = [vtx for vtx in self._adjlist[v].keys() if vtx != '_value']
+            nbs.sort()
+            return nbs
+        return "{} is not in the current graph".format(v)
+
+    def remove_edge(self, a, b):
+        "Removes edge a -> b and b -> a."
+        if a in self._adjlist:
+            if b in self._adjlist[a]:
+                del self._adjlist[a][b]
+        if b in self._adjlist:
+            if a in self._adjlist[b]:
+                del self._adjlist[b][a]
+
+    def remove_vertex(self, v):
+        "Removes a vertex v, also the edges with this vertex."
+        if v in self._adjlist:
+            del self._adjlist[v]
+        for vtx in self._adjlist:
+            if v in self._adjlist[vtx]:
+                del self._adjlist[vtx][v]
+
+    def set_vertex_value(self, v, x):
+        "Sets the value of the vertex v to be x."
+        if v in self._adjlist:
+            self._adjlist[v]['_value'] = x
+
+    def get_vertex_value(self, v):
+        "Returns the value of the vertex v."
+        if v in self._adjlist:
+            return self._adjlist[v]['_value']
+        return "{} is not in the current graph".format(v)
+
+class WeightedGraph(Graph):
+    """
+    A class of weighted undirected graphs.
+    Internal representation:
+        {node: {'_value': None, neighbour1: {'weight': None}, neighbour2: {'weight': None}...}, ...}
+    """
+    def __init__(self, start=None):
+        "Start with an input list of edges or an empty graph, weights are set to be None at first."
+        super().__init__(start)
+    
+    def get_weight(self, a, b):
+        "Returns the weight of an edge a -> b."
+        if a in self._adjlist:
+            if b in self._adjlist[a]:
+                if 'weight' in self._adjlist[a][b]:
+                    return self._adjlist[a][b]['weight']
+                return "The weight between {} and {} has not been set"
+            return "Vertex {} does not have an neighbour {}".format(a,b)
+        return "{} is not in the current graph".format(a)
+    
+    def set_weight(self, a, b, weight):
+        "Sets the weight of edge a -> b."
+        if a in self._adjlist and b in self._adjlist:
+            if a in self._adjlist[b] and b in self._adjlist[a]:
+                self._adjlist[a][b]['weight'] = weight
+                self._adjlist[b][a]['weight'] = weight
+
+
+def dijkstra(graph, source, cost=lambda u,v: 1):
+    """A function to calculate shortest path from the source to all the other vertices. 
+
+    Args:
+        graph (Graph): an input Graph
+        source (string): starting vertex
+        cost (function, optional): the cost function used to calculate the cost between two vertices. Defaults is 1.
+
+    Returns:
+        dict: a dictionary where the keys are all target vertices reachable from the source, 
+              and their values are paths from the source to the target
+    """
+    dist = {vtx: float('inf') for vtx in graph.vertices()}
+    prev = {vtx: None for vtx in graph.vertices() if vtx != source}
+    dist[source] = 0
+    visted_vtx = list()
+    pq = PriorityQueue()
+    pq.put((dist[source], source))
+    # main loop
+    while not pq.empty():
+        _, cur_vtx = pq.get()
+        visted_vtx.append(cur_vtx)
+        
+        for neighbor in graph.neighbours(cur_vtx):
+            weight = cost(cur_vtx, neighbor)
+            if neighbor not in visted_vtx:
+                old_cost = dist[neighbor]
+                new_cost = dist[cur_vtx] + weight
+                if new_cost < old_cost:
+                    pq.put((new_cost, neighbor))
+                    dist[neighbor] = new_cost
+                    prev[neighbor] = cur_vtx
+    # build the output dictionary
+    shortest_path = dict()
+    for vtx in graph.vertices():
+        if vtx != source:
+            possible = True
+            t = vtx
+            path = list()
+            while t != source:
+                if t in prev:
+                    path.append(t)
+                    t = prev[t]
+                else:
+                    possible = False
+                    break
+            if possible:
+                path.append(source)
+                path.reverse()
+                shortest_path[vtx] = path
+    return shortest_path
---
+++ lab3/tram/utils/trams.py
@@ -0,0 +1,270 @@
+import json
+
+# imports added in Lab3 version
+import math
+import os
+from .graphs import WeightedGraph
+from django.conf import settings
+
+
+# path changed from Lab2 version
+# TODO: copy your json file from Lab 1 here
+TRAM_FILE = os.path.join(settings.BASE_DIR, 'static/tramnetwork.json')
+
+
+# TODO: use your lab 2 class definition, but add one method
+class TramStop:
+    '''
+    Define the TramStop class
+
+    Values: name-str: The name of stops
+            lines-list: All the lines which pass this stop
+            position-tuple: The position of stop (lat and lon)
+
+    Function: add_line(line) : Add line to lines
+              get_lines() : Get all lines passing this platform
+              get_name() : Get the name of this stop
+              get_position() : Get the position of this stop
+              set_position(lat, lon) : Set position of this stop
+    ''' 
+    def __init__(self, name, lines = [], lat = None, lon = None):
+        self._name = str(name)
+        self._lines = lines
+        self._position = tuple()
+        self.set_position(lat, lon)
+
+    def add_line(self, line):
+        line = str(line)
+        if line not in self._lines:
+            self._lines.append(line)
+    
+    def remove_line(self, line):
+        line = str(line)
+        if line in self._lines:
+            self._lines.remove(line)
+
+    def get_lines(self):
+        if self._lines:
+            return self._lines
+
+    def get_name(self):
+        return self._name
+
+    def get_position(self):
+        if self._position:
+            return self._position
+
+    def set_position(self, lat, lon):
+        if lat != None and lon != None:
+            self._position = (float(lat), float(lon))
+
+
+class TramLine:
+    '''
+    Define the TramLine class
+
+    Values: number-str: The number of line, but transfer to str
+           stops-list: All stops where line pass
+
+    Function: get_number() : Get the number of this line
+              get_stops() : Get all stops where line pass
+
+    ''' 
+    def __init__(self, num, stops):
+        self._number = str(num)
+        self._stops = list(stops)
+    
+    def get_number(self):
+        return self._number
+
+    def get_stops(self):
+        return self._stops
+
+
+class TramNetwork(WeightedGraph):
+    '''
+    Define the TramNetwork class
+
+    Values: _linedict : A dictory, the key is line(str), the value is the TramLine object
+            _stopdict : A dictory, the key is stop name(str), the value is the TramStop object
+            _timedict : A dictory, the key is stop1 name(str), 
+                       the value is dictory, key is the stop names close to stop1, the value is the time cost
+
+    Function: all_lines(): Get all lines, return list
+              all_stops(): Get all stops, return list
+              extreme_positions(): pass
+              geo_distance(): Get the distance between two stop, return float
+              line_stop(): Get all the stops of this line, return list
+              remove_lines(): Remove one line or some lines from _linedict, _timedict, also call remove_edge function
+              stop_lines(): Get all the lines will stop in this stop, return list
+              stop_position(): Get the position of this stop, Retrun tuple
+              transition_time(a,b): Get the time cost from stop a to stop b
+    ''' 
+    def __init__(self, lines=None, stops=None, times=None):
+        super().__init__()
+
+        self._linedict = dict()
+        if lines:
+            for line in lines:
+                self._linedict[str(line)] = TramLine(line, lines[line])
+
+        self._stopdict = dict()
+        if stops:
+            for stop in stops:
+                _tmp_stop_line = []
+                for line in lines:
+                    if stop in self._linedict[line].get_stops():
+                        _tmp_stop_line.append(line)
+                self._stopdict[str(stop)] = TramStop(stop, lines=_tmp_stop_line, lat=stops[stop]['lat'], lon=stops[stop]['lon'])
+
+        self._timedict = dict()
+        if times:
+            self._timedict = times
+        # Initialize weighted graph
+        for stop in self.all_stops():
+            self.add_vertex(stop)
+        for stop1 in self._timedict:
+            for stop2 in self._timedict[stop1]:
+                self.add_edge(stop1, stop2)
+                self.set_weight(stop1, stop2, self._timedict[stop1][stop2])
+                
+    
+    def all_lines(self):
+        return list(self._linedict.keys())
+
+    def all_stops(self):
+        return list(self._stopdict.keys())
+
+    def extreme_positions(self):
+        pass
+    
+    def geo_distance(self, a, b):
+        a = str(a)
+        b = str(b)
+
+        if a not in list(self._stopdict.keys()) or b not in list(self._stopdict.keys()):
+            return "unknown arguments"
+        if a == b:
+            return("Please enter two different stops!")
+        
+        lat1, lon1 = self.stop_position(a)
+        lat2, lon2 = self.stop_position(b)
+        
+        Earth_Radius = 6371.009
+        one_degree = math.pi/180
+        lat1 *= one_degree
+        lon1 *= one_degree
+        lat2 *= one_degree
+        lon2 *= one_degree
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        lat_m = (lat1 + lat2)/2
+
+        distance = Earth_Radius * math.sqrt(dlat**2 + (math.cos(lat_m) * dlon)**2)
+        return round(distance, 3)
+
+    def line_stops(self, line):
+        line = str(line)
+        if line in list(self._linedict.keys()):
+            return self._linedict[line].get_stops()
+        return("The line {} is not exist".format(line))
+
+    def remove_lines(self, lines):
+        for line in lines:
+            line = str(line)
+            line_stops = self.line_stops(line)
+            
+            # update _linedict, remove the whole line
+            if line in list(self._linedict.keys()):
+                del self._linedict[line]
+            
+            # update _stopdict, remove the line from stop's info
+            for stop in line_stops:
+                if line in self._stopdict[stop].get_lines():
+                    self._stopdict[stop].remove_line(line)
+            
+            # update _timedict
+            # if no other lines passing through adjacent stops in the deleting line, delete time info
+            # and also delete this edge from edges
+            for i, stop in enumerate(line_stops):
+                delete = True
+                if i == len(line_stops)-2:
+                    break
+                next_stop = line_stops[i+1]
+                for other_line in self.all_lines():
+                    other_line_stops = self.line_stops(other_line)
+                    for _i, _stop in enumerate(other_line_stops):
+                        if _i == len(other_line_stops)-2:
+                            break
+                        _next_stop = other_line_stops[_i+1]
+                        if (stop == _stop and next_stop == _next_stop) or (stop == _next_stop and next_stop == _stop):
+                            delete = False
+                            break
+                    if not delete:
+                        break
+                if delete:
+                    self.remove_edge(stop, next_stop)
+                    del self._timedict[stop][next_stop]
+                    del self._timedict[next_stop][stop]
+                    
+
+    def stop_lines(self, a):
+        if a in self._stopdict:
+            return self._stopdict[a].get_lines()
+        
+    def stop_position(self, a):
+        if a in self._stopdict:
+            return self._stopdict[a].get_position()
+
+    def transition_time(self, a, b):
+        if a in list(self._timedict.keys()):
+            if b in list(self._timedict[a].keys()):
+                return self._timedict[a][b]
+            return '{} and {} are not adjacent'.format(a,b)
+    
+    def extreme_positions(self):
+        stops = self._stopdict.values()
+        minlat = min([s._position[0] for s in stops])
+        maxlat = max([s._position[0] for s in stops])
+        minlon = min([s._position[1] for s in stops])
+        maxlon = max([s._position[1] for s in stops])
+        return minlon, minlat, maxlon, maxlat
+
+
+def readTramNetwork(tramfile=TRAM_FILE):
+    '''
+    Define the readTramNetwork function
+
+    Input: the path of json file
+
+    Output: a object of tramnetwork
+    ''' 
+    with open(tramfile, 'r', encoding='utf-8') as jfile:
+        tramnetwork = json.load(jfile)
+
+    lines = tramnetwork['lines']
+    stops = tramnetwork['stops']
+    times = tramnetwork['times']
+
+    tramnetwork =  TramNetwork(lines, stops, times)
+    return tramnetwork
+
+
+# Bonus task 1: take changes into account and show used tram lines
+
+def specialize_stops_to_lines(network):
+    # TODO: write this function as specified
+    return network
+
+
+def specialized_transition_time(spec_network, a, b, changetime=10):
+    # TODO: write this function as specified
+    return changetime
+
+
+def specialized_geo_distance(spec_network, a, b, changedistance=0.02):
+    # TODO: write this function as specified
+    return changedistance
+
+
